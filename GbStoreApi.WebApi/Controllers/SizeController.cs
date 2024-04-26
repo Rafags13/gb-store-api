69004a4ecfeaf376981a920db5f74aab49292908
Rafael Veiga <rafael_todescato@hotmail.com>
2024-04-26 09:25:34 -0400

    feat: added endpoints to find size by name and its id.

--- GbStoreApi.WebApi/Controllers/SizeController.cs
+++ GbStoreApi.WebApi/Controllers/SizeController.cs
@@ -1,6 +1,7 @@
 ﻿using GbStoreApi.Application.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
 
 namespace GbStoreApi.WebApi.Controllers
 {
@@ -26,6 +27,20 @@ namespace GbStoreApi.WebApi.Controllers
         public IActionResult Create([FromBody] string sizeName)
         {
             var response = _sizeService.Create(sizeName);
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            var response = _sizeService.GetById(id);
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetById([FromRoute] string name)
+        {
+            var response = _sizeService.GetByName(name);
             return StatusCode(response.StatusCode, response);
         }
     }
